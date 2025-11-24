078f09ee71edfaef472899a2f2609df3b951804a
Manikandan <manikandanrajappa1514@gmail.com>
2025-11-24 11:15:22 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,30 +1,49 @@
-while True:
-    print("\n--- Calculator Menu ---")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Exit")
+# Constants for messages
+MENU_TEXT = """
+--- Calculator Menu ---
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Exit
+"""
 
-    choice = input("Choose an option (1-5): ")
+PROMPT_CHOICE = "Choose an option (1-5): "
+PROMPT_NUM1 = "Enter first number: "
+PROMPT_NUM2 = "Enter second number: "
+ERROR_DIV_ZERO = "Error: Division by zero!"
+ERROR_INVALID_CHOICE = "Invalid choice!"
+GOODBYE_MSG = "Goodbye!"
+RESULT_MSG = "Result: {}"
+
+# Calculator loop
+while True:
+    print(MENU_TEXT)
+    choice = input(PROMPT_CHOICE)
 
     if choice == "5":
-        print("Goodbye!")
+        print(GOODBYE_MSG)
         break
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    # Get numbers from user
+    try:
+        num1 = float(input(PROMPT_NUM1))
+        num2 = float(input(PROMPT_NUM2))
+    except ValueError:
+        print("Error: Please enter a valid number!")
+        continue
 
+    # Perform calculation
     if choice == "1":
-        print("Result:", num1 + num2)
+        print(RESULT_MSG.format(num1 + num2))
     elif choice == "2":
-        print("Result:", num1 - num2)
+        print(RESULT_MSG.format(num1 - num2))
     elif choice == "3":
-        print("Result:", num1 * num2)
+        print(RESULT_MSG.format(num1 * num2))
     elif choice == "4":
         if num2 == 0:
-            print("Error: Division by zero!")
+            print(ERROR_DIV_ZERO)
         else:
-            print("Result:", num1 / num2)
+            print(RESULT_MSG.format(num1 / num2))
     else:
-        print("Invalid choice!")
+        print(ERROR_INVALID_CHOICE)
